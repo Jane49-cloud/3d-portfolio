f9731855259d75e38e3245a11b483b7293fe56f1
Jane49-cloud <janendirangu49@gmail.com>
2024-01-17 02:02:26 +0300

    debug

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -19,7 +19,7 @@ const Tech = () => {
       <div className="flex flex-wrap justify-center lg:justify-start -mx-2">
         {technologies.map((technology) => (
           <div
-            className="w-[40vw] md:w-1/2 lg:w-1/3 xl:w-1/4 px-2 mb-4"
+            className="w-[43vw] md:w-1/2 lg:w-1/3 xl:w-1/4 px-2 mb-4"
             key={technology.name}
           >
             <div
